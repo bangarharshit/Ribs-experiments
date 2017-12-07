b9a3809f3cf5bbb089c5587b69e589055658c6d0
Harshit Bangar <bangarh@uber.com>
2017-12-07 14:07:49 +0530

    Fixed code

--- app/src/main/java/com/uber/rib/core/NonInjectableInteractor.java
+++ app/src/main/java/com/uber/rib/core/NonInjectableInteractor.java
@@ -1,6 +1,6 @@
 package com.uber.rib.core;
 
-public abstract class ≠<P, R extends Router> extends Interactor<P, R> {
+public abstract class NonInjectableInteractor<P, R extends Router> extends Interactor<P, R> {
 
   // Hack we need it for injecting presenter since without dagger it is getting called manually.
   public void setPresenter(P p) {
